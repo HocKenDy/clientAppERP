cb00ceef40890a983cb14e6096cfbb4e49678869
Nguyễn Thái Học <nguyenthaihoc.hcmus.2014@gmail.com>
2020-06-05 20:20:23 +0700

    First commit

---
+++ clientERP/src/app/_models/product.ts
@@ -0,0 +1,10 @@
+export class Product {
+    id: any;
+    name: string;
+    categoryid: string;
+    image: string;
+    price: string;
+    description: string;
+    content: string;
+    unit: string
+}
---
+++ clientERP/src/app/_models/user.ts
@@ -0,0 +1,12 @@
+export class User {
+    id: any;
+    fullName: string;
+    birthday: string;
+    email: string;
+    password: string;
+    username: string;
+    address: string;
+    phonenumber: string;
+    gender: string;
+    avatar: string;
+}
